3a9e62fba529934a7f063526b193653728047e24
Romerito Campos <romerito.campos@gmail.com>
2024-03-18 16:53:07 -0300

    Novas páginas adicionadas ao exemplo

---
+++ slides/04_forms/exemplos/caso01/app.py
@@ -0,0 +1,43 @@
+from flask import Flask, request, render_template, abort
+from faker import Faker
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/login', methods=['POST', 'GET'])
+def login():
+    if request.method == 'GET':
+        return render_template('login.html')
+    elif request.method == 'POST':
+        email = request.form['email']
+        senha = request.form['senha']
+
+        if email == 'admin@email.com' and senha == '123123':
+            return render_template('dashboard')
+        else:
+            return render_template('errors/401.html'), 401
+        
+
+@app.route('/dashboard/<nome>')
+def dashboard(nome):
+    return render_template('dashboard.html',nome=nome)
+
+@app.route('/buscar')
+def search():
+    lista = ['livro', 'pc', 'camisa', 'relogio']
+    item = request.args.get('item')
+    resultado = {}
+    #obter string de consulta
+    if item in lista:
+        faker = Faker(locale="pt_BR")
+        for x in lista:
+            resultado[x] = [ faker.job() for y in range(50) ]
+        return render_template('produtos.html', produtos=resultado[item])
+    else:
+        return render_template('errors/400.html'), 400
+        
+
+    
