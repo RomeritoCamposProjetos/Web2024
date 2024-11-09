01de25a1fd6bd1bc882a159e5415080d53df326c
Romerito Campos <romerito.campos@gmail.com>
2024-11-09 11:09:13 -0300

    resposta - caso1 adicionada

--- avaliacoes/b3/case1/auth/bp.py
+++ avaliacoes/b3/case1/auth/bp.py
@@ -1,40 +1,36 @@
-from flask import Blueprint, request, redirect, url_for, render_template
-from flask_login import logout_user, login_user, LoginManager, login_required
-from models.user import User
+from flask import Blueprint, redirect, url_for, \
+    request, render_template
 
-# definir login_manager
+from flask_login import LoginManager, login_required, login_user, logout_user
+
+from users.models import User
+
 login_manager = LoginManager()
 
 @login_manager.user_loader
 def load_user(user_id):
     return User.find(id=user_id)
 
-# criar bluprint de auth
 auth_bp = Blueprint(
-    name='auth', 
+    name='auth',
     import_name=__name__,
-    url_prefix='/auth',
     template_folder='templates')
 
-# definir rota para login
 @auth_bp.route('/login', methods=['POST', 'GET'])
 def login():
     if request.method == 'POST':
         email = request.form['email']
+        nome = request.form['nome']
+
         user = User.find(email=email)
         if user:
             login_user(user)
             return redirect(url_for('users.index'))
+        
     return render_template('auth/login.html')
 
-#definir rota para logout
-# logout apenas com usuários logados
-# apenas método post
 @auth_bp.route('/logout', methods=['POST'])
 @login_required
 def logout():
     logout_user()
     return redirect(url_for('index'))
-
-
-
--- avaliacoes/b3/case2/app.py
+++ avaliacoes/b3/case2/app.py
@@ -1,17 +1,17 @@
 from flask import Flask
 from controllers import users, books
 
-# importar o bluprint e o login manager
-from auth.bp import auth_bp, login_manager
+# importar o bluprint e o login manager (PROVA)
+
 
 app = Flask(__name__)
 
-# configurar secret key
-app.config['SECRET_KEY'] = 'dificil'
-# inicializar o app no login manager
-login_manager.init_app(app)
-# registrando o blueprint
-app.register_blueprint(auth_bp)
+# configurar secret key (PROVA)
+
+# inicializar o app no login manager (PROVA)
+
+# registrando o blueprint (PROVA)
+
 
 app.register_blueprint(users.bp)
 app.register_blueprint(books.bp)
